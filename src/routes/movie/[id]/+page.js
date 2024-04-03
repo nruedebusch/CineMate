50db01080acfe0c7f3a043aa207da3b95f38e5ab
nruedebusch <140553125+nruedebusch@users.noreply.github.com>
2024-04-03 20:13:23 +0200

    Update +page.js

--- src/routes/movie/[id]/+page.js
+++ src/routes/movie/[id]/+page.js
@@ -1,5 +1,5 @@
 export async function load({ params }) {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=0cfd029479ad3a9da0ed691331abd850`);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${import.meta.env.VITE_API_KEY}`);
     const movie = await response.json();
     return {
       movie,
